0cb49fd90bf58848859a1e8626cf88ee3158b132
Robert Slifka <robert.slifka@gmail.com>
2017-12-04 07:51:40 -0800

    Add detail to exclusion reasoning

--- brunch-config.js
+++ brunch-config.js
@@ -9,7 +9,13 @@ exports.files = {
   }
 };
 
-// Tests are handled by Karma
+/*
+  1. Tests are handled by Karma. This is to silence a warning that Brunch
+     reports (which is helpful actually in most cases!) because it sees JS files
+     outside the scope of its purview.
+  2. We exclude them because we don't want them bundled so that the Karma
+     watcher only re-runs the tests we've changed during active development.
+*/
 exports.conventions = {
   ignored: /^test/
 };
