52ad13a66ced699c549b0d6e828d4b9f4d4712ce
Robert Slifka <robert.slifka@gmail.com>
2017-12-10 14:49:00 -0800

    Init in production

--- app/main.js
+++ app/main.js
@@ -3,3 +3,10 @@ require('configuration.js');
 
 // Apply our custom CSS
 require('beautification.js');
+
+const logger = require('logger');
+const postal = require('postal');
+if (process.env.NODE_ENV === 'production') {
+  logger.log('Initializing');
+  postal.publish({topic:'fate.init'});
+}
