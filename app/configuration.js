f3fc02020cff19343a0cb2e4fe1e7c7648c21aa5
Robert Slifka <robert.slifka@gmail.com>
2017-12-10 10:35:25 -0800

    Move to eventing

--- app/configuration.js
+++ app/configuration.js
@@ -1,27 +1,33 @@
 const $ = require('jquery');
 const logger = require('logger');
+const postal = require('postal');
 
-logger.log('Initializing GM_config');
+postal.subscribe({
+	topic: 'fate.init',
+	callback: function() {
+    logger.log('Initializing GM_config');
 
-GM_config.init({
-  'id': 'FoafConfig',
-  'fields': {
-    'weaponDataTSV': {
-      'label': 'Weapon Data Tab-Separated Values',
-      'type': 'text',
-      'default': 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQ06pCDSdvu2nQzgHMXl22ci-6pO9rTTmvZmlKXaiBrIHVhl1X1awIaHEOagZcs4ME4X9ZMEghBP9NE/pub?gid=2031623180&single=true&output=tsv'
-    }
-  },
-  'events': {
-    'save': function() {
-      // TODO - Emit an event?
-      // rankingsDownloaded();
-    }
+    GM_config.init({
+      'id': 'FoafConfig',
+      'fields': {
+        'weaponDataTSV': {
+          'label': 'Weapon Data Tab-Separated Values',
+          'type': 'text',
+          'default': 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQ06pCDSdvu2nQzgHMXl22ci-6pO9rTTmvZmlKXaiBrIHVhl1X1awIaHEOagZcs4ME4X9ZMEghBP9NE/pub?gid=2031623180&single=true&output=tsv'
+        }
+      },
+      'events': {
+        'save': function() {
+          // TODO - Emit an event?
+          // rankingsDownloaded();
+        }
+      }
+    });
+
+    $('body').append($("<div>", {"class": "foaf-config"}).text('[FOAF Config]'));
+
+    $('.foaf-config').on('click', function() {
+      GM_config.open();
+    });
   }
-});
-
-$('body').append($("<div>", {"class": "foaf-config"}).text('[FOAF Config]'));
-
-$('.foaf-config').on('click', function() {
-  GM_config.open();
 });
--- spec/javascripts/configuration.spec.js
+++ spec/javascripts/configuration.spec.js
@@ -1,19 +1,35 @@
 describe('configuration.js', function() {
 
+  const postal = require('postal');
+
   describe('initialization', function() {
 
-    it('should use Slifs spreadsheet by default', function() {
-      expect(GM_config.get('weaponDataTSV')).toBe('https://docs.google.com/spreadsheets/d/e/2PACX-1vQ06pCDSdvu2nQzgHMXl22ci-6pO9rTTmvZmlKXaiBrIHVhl1X1awIaHEOagZcs4ME4X9ZMEghBP9NE/pub?gid=2031623180&single=true&output=tsv');
+    it('should register configuration tooling', function() {
+      spyOn(GM_config, 'init');
+      postal.publish({topic:'fate.init'});
+      expect(GM_config.init).toHaveBeenCalled();
     });
 
-    it('should register a handler', function() {
-      const spyEvent = spyOnEvent('.foaf-config', 'click');
-      spyOn(GM_config, 'open');
+    describe('defaults and attachment handling', function() {
 
-      $('.foaf-config').click();
+      beforeEach(function() {
+        postal.publish({topic:'fate.init'});
+      });
 
-      expect(spyEvent).toHaveBeenTriggered();
-      expect(GM_config.open).toHaveBeenCalled();
+      it('should use Slifs spreadsheet by default', function() {
+        expect(GM_config.get('weaponDataTSV')).toBe('https://docs.google.com/spreadsheets/d/e/2PACX-1vQ06pCDSdvu2nQzgHMXl22ci-6pO9rTTmvZmlKXaiBrIHVhl1X1awIaHEOagZcs4ME4X9ZMEghBP9NE/pub?gid=2031623180&single=true&output=tsv');
+      });
+
+      it('should register a handler', function() {
+        const spyEvent = spyOnEvent('.foaf-config', 'click');
+        spyOn(GM_config, 'open');
+
+        $('.foaf-config').click();
+
+        expect(spyEvent).toHaveBeenTriggered();
+        expect(GM_config.open).toHaveBeenCalled();
+      });
+
     });
 
   });
