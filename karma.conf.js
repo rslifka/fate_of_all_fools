ce8fcc02019d55d27519e8222d2743076da4cff3
Robert Slifka <robert.slifka@gmail.com>
2017-12-16 06:09:13 -0800

    Include tampermonkey overrides first, all others after

--- karma.conf.js
+++ karma.conf.js
@@ -3,10 +3,9 @@ module.exports = function(config) {
     frameworks: ['jquery-3.2.1', 'jasmine-jquery', 'jasmine'],
     browsers: ['ChromeHeadless'],
     files: [
-      // Helpers included before the application so that we can stub out
-      // functionality from its container (i.e. Tampermonkey).
-      'spec/helpers/*.js',
+      'spec/helpers/tampermonkeyStubs.js',
       'public/fateOfAllFools.js',
+      'spec/helpers/!(tampermonkeyStubs).js',
       {pattern: 'spec/javascripts/fixtures/*.html', included: false},
       'spec/javascripts/*.spec.js'
     ]
