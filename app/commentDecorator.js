68ca0746afb084b1666385a3999f41a73ab55014
Robert Slifka <robert.slifka@gmail.com>
2017-12-17 12:57:54 -0800

    Store original weapon name for later use

--- app/commentDecorator.js
+++ app/commentDecorator.js
@@ -5,14 +5,19 @@ const weaponDatabase = require('weaponDatabase.js');
 
 function updateTitles() {
   logger.log('commentDecorator.js: Updating weapon tooltips');
-  $('[drag-channel=Kinetic],[drag-channel=Energy],[drag-channel=Power]').each(function(index,element) {
-    const weaponName = $(this).attr('title');
+  $('[drag-channel=Kinetic],[drag-channel=Energy],[drag-channel=Power]').not('[data-fate-weapon-name]').each(function(index,element) {
+    $(this).attr('data-fate-weapon-name', $(this).attr('title'));
+  });
+  $('[data-fate-weapon-name]').each(function(index,element) {
+    const weaponName = $(this).attr('data-fate-weapon-name');
     if (!weaponDatabase.contains(weaponName)) {
       return true;
     }
     var weapon = weaponDatabase.get(weaponName);
-    const tooltipText = weaponName + ' // ' + weapon.type + ' - ' + weapon.subtype + '\n' + weapon.comments;
-    $(this).attr('title', tooltipText);
+    $(this).attr(
+      'title',
+      weaponName + ' // ' + weapon.type + ' - ' + weapon.subtype + '\n' + weapon.comments
+    );
   });
 }
 
--- spec/javascripts/commentDecorator.spec.js
+++ spec/javascripts/commentDecorator.spec.js
@@ -12,36 +12,52 @@ describe('commentDecorator.js', function() {
   });
 
   describe('in response to fate.refresh', function() {
-    it('should replace the DIM tooltip with our comment', function() {
-      spyOn(weaponDatabase, 'contains').and.returnValue(true);
-      spyOn(weaponDatabase, 'get').and.callFake(function(weaponName) {
-        switch(weaponName) {
-          case 'Origin Story':
-            return {name: 'Origin Story', type: 'Auto Rifle', subtype: 'Precision', comments: 'Rampage makes this great for lots of adds killing'};
-          case 'Annual Skate':
-            return {name: 'Annual Skate', type: 'Hand Cannon', subtype: 'Adaptive', comments: 'Seems strictly worse than the Minuet?'};
-          case 'Alone as a god':
-            return {name: 'Alone as a god', type: 'Sniper Rifle', subtype: 'Rapid-Fire', comments: 'TT, fast ADS, fast reload, awesome'};
-        }
+
+    describe('when the weapons are known', function() {
+
+      beforeEach(function() {
+        spyOn(weaponDatabase, 'contains').and.returnValue(true);
+        spyOn(weaponDatabase, 'get').and.callFake(function(weaponName) {
+          switch(weaponName) {
+            case 'Origin Story':
+              return {name: 'Origin Story', type: 'Auto Rifle', subtype: 'Precision', comments: 'Rampage makes this great for lots of adds killing'};
+            case 'Annual Skate':
+              return {name: 'Annual Skate', type: 'Hand Cannon', subtype: 'Adaptive', comments: 'Seems strictly worse than the Minuet?'};
+            case 'Alone as a god':
+              return {name: 'Alone as a god', type: 'Sniper Rifle', subtype: 'Rapid-Fire', comments: 'TT, fast ADS, fast reload, awesome'};
+          }
+        });
+        postal.publish({topic:'fate.refresh'});
       });
 
-      postal.publish({topic:'fate.refresh'});
+      it('should replace the DIM tooltip with our comment', function() {
+        expect($('[drag-channel=Kinetic]')).toHaveAttr('title', "Origin Story // Auto Rifle - Precision\nRampage makes this great for lots of adds killing");
+        expect($('[drag-channel=Energy]')).toHaveAttr('title', "Annual Skate // Hand Cannon - Adaptive\nSeems strictly worse than the Minuet?");
+        expect($('[drag-channel=Power]')).toHaveAttr('title', "Alone as a god // Sniper Rifle - Rapid-Fire\nTT, fast ADS, fast reload, awesome");
+      });
 
-      expect($('[drag-channel=Kinetic]')).toHaveAttr('title', `Origin Story // Auto Rifle - Precision
-Rampage makes this great for lots of adds killing`);
-      expect($('[drag-channel=Energy]')).toHaveAttr('title', `Annual Skate // Hand Cannon - Adaptive
-Seems strictly worse than the Minuet?`);
-      expect($('[drag-channel=Power]')).toHaveAttr('title', `Alone as a god // Sniper Rifle - Rapid-Fire
-TT, fast ADS, fast reload, awesome`);
+      it('should store the original weapon name', function() {
+        expect($('[drag-channel=Kinetic]')).toHaveAttr('data-fate-weapon-name', 'Origin Story');
+        expect($('[drag-channel=Energy]')).toHaveAttr('data-fate-weapon-name', 'Annual Skate');
+        expect($('[drag-channel=Power]')).toHaveAttr('data-fate-weapon-name', 'Alone as a god');
+      });
+
+      it('should not overwrite the original weapon name on further refreshes', function() {
+        postal.publish({topic:'fate.refresh'});
+        expect($('[drag-channel=Kinetic]')).toHaveAttr('data-fate-weapon-name', 'Origin Story');
+        expect($('[drag-channel=Energy]')).toHaveAttr('data-fate-weapon-name', 'Annual Skate');
+        expect($('[drag-channel=Power]')).toHaveAttr('data-fate-weapon-name', 'Alone as a god');
+      });
+
     });
 
-    it('should ignore weapons it does not know about', function() {
-      postal.publish({topic:'fate.refresh'});
-      expect($('[drag-channel=Kinetic]')).toHaveAttr('title', 'Origin Story');
-      expect($('[drag-channel=Energy]')).toHaveAttr('title', 'Annual Skate');
-      expect($('[drag-channel=Power]')).toHaveAttr('title', 'Alone as a god');
-    })
-
+    describe('when the weapons are not known', function() {
+      it('should store the original weapon name', function() {
+        postal.publish({topic:'fate.refresh'});
+        expect($('[drag-channel=Kinetic]')).toHaveAttr('data-fate-weapon-name', 'Origin Story');
+        expect($('[drag-channel=Energy]')).toHaveAttr('data-fate-weapon-name', 'Annual Skate');
+        expect($('[drag-channel=Power]')).toHaveAttr('data-fate-weapon-name', 'Alone as a god');
+      })
+    });
   });
-
 });
