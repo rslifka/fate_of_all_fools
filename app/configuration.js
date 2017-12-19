42cf8d46470719bb6bf37e8cd28cfa4c69f80db7
Robert Slifka <robert.slifka@gmail.com>
2017-12-18 19:01:00 -0800

    Using fateBus for subscription

--- app/configuration.js
+++ app/configuration.js
@@ -1,33 +1,34 @@
 const $ = require('jquery');
 const logger = require('logger');
-const postal = require('postal');
 
-postal.subscribe({
+function init() {
+	logger.log('configuration.js: Initializing');
+
+	GM_config.init({
+		'id': 'FoafConfig',
+		'fields': {
+			'weaponDataTSV': {
+				'label': 'Weapon Data Tab-Separated Values',
+				'type': 'text',
+				'default': 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQ06pCDSdvu2nQzgHMXl22ci-6pO9rTTmvZmlKXaiBrIHVhl1X1awIaHEOagZcs4ME4X9ZMEghBP9NE/pub?gid=2031623180&single=true&output=tsv'
+			}
+		},
+		'events': {
+			'save': function() {
+				// TODO - Emit an event?
+				// rankingsDownloaded();
+			}
+		}
+	});
+
+	$('body').append($("<div>", {"class": "foaf-config"}).text('[FATE Config]'));
+
+	$('.foaf-config').on('click', function() {
+		GM_config.open();
+	});
+}
+
+fateBus.subscribe(module, {
 	topic: 'fate.init',
-	callback: function() {
-    logger.log('configuration.js: Initializing');
-
-    GM_config.init({
-      'id': 'FoafConfig',
-      'fields': {
-        'weaponDataTSV': {
-          'label': 'Weapon Data Tab-Separated Values',
-          'type': 'text',
-          'default': 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQ06pCDSdvu2nQzgHMXl22ci-6pO9rTTmvZmlKXaiBrIHVhl1X1awIaHEOagZcs4ME4X9ZMEghBP9NE/pub?gid=2031623180&single=true&output=tsv'
-        }
-      },
-      'events': {
-        'save': function() {
-          // TODO - Emit an event?
-          // rankingsDownloaded();
-        }
-      }
-    });
-
-    $('body').append($("<div>", {"class": "foaf-config"}).text('[FATE Config]'));
-
-    $('.foaf-config').on('click', function() {
-      GM_config.open();
-    });
-  }
+	callback: init
 });
