1fad344c4348081118ee69d758f6ebf5ee75aa72
Robert Slifka <robert.slifka@gmail.com>
2017-12-18 17:40:03 -0800

    Our message bus now wraps subscription as well

--- app/fateBus.js
+++ app/fateBus.js
@@ -6,14 +6,37 @@ function registerModule(brunchModule) {
   muteStatus.set(brunchModule.id, false);
 }
 
+function deregisterModules() {
+  muteStatus.clear();
+}
+
+function isMuted(brunchModule) {
+  return muteStatus.get(brunchModule.id);
+}
+
 function publish(brunchModule, postalData) {
-  if (muteStatus.has(brunchModule.id)) {
-    if (!muteStatus.get(brunchModule.id)) {
-      postal.publish(postalData);
-    }
-  } else {
-    throw new Error('Module ['+brunchModule.id+'] is not defined');
+  if (!muteStatus.has(brunchModule.id)) {
+    throw new Error('fateBus.js#publish: Module ['+brunchModule.id+'] is not defined');
+    return;
   }
+  if (!isMuted(brunchModule)) {
+    postal.publish(postalData);
+  }
+}
+
+function subscribe(brunchModule, postalData) {
+  if (!muteStatus.has(brunchModule.id)) {
+    throw new Error('fateBus.js#subscribe: Module ['+brunchModule.id+'] is not defined');
+    return;
+  }
+  postal.subscribe({
+    topic: postalData.topic,
+    callback: function(data, envelope) {
+      if (!isMuted(brunchModule)) {
+        postalData.callback(data, envelope);
+      }
+    }
+  });
 }
 
 function mute(moduleId) {
@@ -32,7 +55,9 @@ function unmute(moduleId) {
 }
 
 exports.registerModule = registerModule;
+exports.deregisterModules = deregisterModules;
 exports.publish = publish;
+exports.subscribe = subscribe;
 exports.mute = mute;
 exports.muteAll = muteAll;
 exports.unmute = unmute;
--- spec/javascripts/fateBus.spec.js
+++ spec/javascripts/fateBus.spec.js
@@ -3,6 +3,10 @@ describe('fateBus.js', function() {
   const fateBus = require('fateBus.js');
   const postal = require('postal');
 
+  beforeEach(function() {
+    fateBus.deregisterModules();
+  });
+
   describe('when a module is registered', function() {
     it('should be allowed to publish', function() {
       spyOn(postal, 'publish');
@@ -12,34 +16,73 @@ describe('fateBus.js', function() {
     });
   });
 
-  describe('when a module is not registered and publishes', function() {
-    it('should be an error', function() {
-      spyOn(postal, 'publish');
-      expect(function() {
-        fateBus.publish({id:'UNREGISTERED_MODULE.JS'}, '_');
-      }).toThrowError(Error, 'Module [UNREGISTERED_MODULE.JS] is not defined');
-      expect(postal.publish).not.toHaveBeenCalled();
+  describe('when a module is not registered', function() {
+
+    describe('and attempts to publish', function() {
+      it('should be an error', function() {
+        spyOn(postal, 'publish');
+        expect(function() {
+          fateBus.publish({id:'UNREGISTERED_MODULE.JS'}, '_');
+        }).toThrowError(Error, 'fateBus.js#publish: Module [UNREGISTERED_MODULE.JS] is not defined');
+        expect(postal.publish).not.toHaveBeenCalled();
+      });
     });
+
+    describe('and attempts to subscribe', function() {
+      it('should be an error', function() {
+        expect(function() {
+          fateBus.subscribe({id:'UNREGISTERED_MODULE.JS'}, '_');
+        }).toThrowError(Error, 'fateBus.js#subscribe: Module [UNREGISTERED_MODULE.JS] is not defined');
+      });
+    });
+
   });
 
   describe('when a module is muted', function() {
-    it('should not be allowed to publish', function() {
-      spyOn(postal, 'publish');
+
+    beforeEach(function() {
       fateBus.registerModule({id:'MUTED_MODULE.JS'});
       fateBus.mute('MUTED_MODULE.JS');
+    });
+
+    it('should not be allowed to publish', function() {
+      spyOn(postal, 'publish');
       fateBus.publish({id:'MUTED_MODULE.JS'}, '_');
       expect(postal.publish).not.toHaveBeenCalled();
     });
+
+    it('should not respond to events', function() {
+      let postalData = {topic: 'TEST_TOPIC', callback: function(){}};
+      spyOn(postalData, 'callback');
+      fateBus.subscribe({id:'MUTED_MODULE.JS'}, postalData);
+      postal.publish({topic:'TEST_TOPIC',data:'TEST_DATA'});
+      expect(postalData.callback).not.toHaveBeenCalled();
+    });
+
   });
 
   describe('when a module is unmuted', function() {
+
     it('should be allowed to publish', function() {
       spyOn(postal, 'publish');
       fateBus.registerModule({id:'TOGGLE_MODULE.JS'});
       fateBus.mute('TOGGLE_MODULE.JS');
       fateBus.unmute('TOGGLE_MODULE.JS');
-      fateBus.publish({id:'TOGGLE_MODULE.JS'}, '_');
-      expect(postal.publish).toHaveBeenCalled();
+      fateBus.publish({id:'TOGGLE_MODULE.JS'}, {topic:'TEST_TOPIC',data:'TEST_DATA'});
+      expect(postal.publish).toHaveBeenCalledWith({topic:'TEST_TOPIC',data:'TEST_DATA'});
+    });
+
+    it('should repsond to events', function() {
+      it('should not respond to events', function() {
+        fateBus.registerModule({id:'TEST_PUBLISHER.JS'});
+        fateBus.unmute('TEST_PUBLISHER.JS');
+
+        let postalData = {topic: 'TEST_TOPIC', callback: function(){}};
+        spyOn(postalData, 'callback');
+        fateBus.subscribe({id:'TEST_SUBSCRIBER.JS'}, postalData);
+        fateBus.publish({id:'TEST_PUBLISHER.JS'}, {topic:'TEST_TOPIC',data:'TEST_DATA'});
+        expect(postalData.callback).toHaveBeenCalledWith({topic:'TEST_TOPIC',data:'TEST_DATA'});
+      });
     });
   });
 
