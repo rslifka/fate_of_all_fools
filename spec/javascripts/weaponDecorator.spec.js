77cc195ff2e77c140308657023ca4d518445376e
Robert Slifka <robert.slifka@gmail.com>
2017-12-20 06:11:55 -0800

    Updated to fateBus wrapper

--- spec/javascripts/weaponDecorator.spec.js
+++ spec/javascripts/weaponDecorator.spec.js
@@ -1,6 +1,11 @@
 describe('weaponDecorator.js', function() {
 
-  const postal = require('postal');
+  const fateBus = require('fateBus.js');
+  const brunchModule = {id:'test'+this.result.description};
+
+  beforeEach(function() {
+    fateBus.registerModule(brunchModule);
+  });
 
   describe('in response to fate.refresh', function() {
 
@@ -10,7 +15,7 @@ describe('weaponDecorator.js', function() {
         'energyWeaponRaw.html',
         'powerWeaponRaw.html'
       );
-      postal.publish({topic:'fate.refresh'});
+      fateBus.publish(brunchModule, 'fate.refresh');
     });
 
     it('should store the original weapon name', function() {
@@ -23,7 +28,7 @@ describe('weaponDecorator.js', function() {
       $('[drag-channel=Kinetic]').attr('title', '_');
       $('[drag-channel=Energy]').attr('title', '_');
       $('[drag-channel=Power]').attr('title', '_');
-      postal.publish({topic:'fate.refresh'});
+      fateBus.publish(brunchModule, 'fate.refresh');
       expect($('[drag-channel=Kinetic]')).toHaveAttr('data-fate-weapon-name', 'Origin Story');
       expect($('[drag-channel=Energy]')).toHaveAttr('data-fate-weapon-name', 'Annual Skate');
       expect($('[drag-channel=Power]')).toHaveAttr('data-fate-weapon-name', 'Alone as a god');
