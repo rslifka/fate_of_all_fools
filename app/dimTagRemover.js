01cfb14adc144acb330ab39fa6e1f901d44de327
Robert Slifka <robert.slifka@gmail.com>
2017-12-19 20:01:19 -0800

    Updated to fateBus wrapper

--- app/dimTagRemover.js
+++ app/dimTagRemover.js
@@ -1,10 +1,7 @@
 const $ = require('jquery');
 const logger = require('logger');
 
-fateBus.subscribe(module, {
-	topic: 'fate.refresh',
-	callback: function() {
-    logger.log('dimTagRemover.js: Removing DIM tagging elements');
-    $('[drag-channel=Kinetic],[drag-channel=Energy],[drag-channel=Power],[drag-channel=Helmet],[drag-channel=Gauntlets],[drag-channel=Chest],[drag-channel=Leg],[drag-channel=ClassItem]').children('.item-tag').remove();
-  }
+fateBus.subscribe(module, 'fate.refresh', function() {
+  logger.log('dimTagRemover.js: Removing DIM tagging elements');
+  $('[drag-channel=Kinetic],[drag-channel=Energy],[drag-channel=Power],[drag-channel=Helmet],[drag-channel=Gauntlets],[drag-channel=Chest],[drag-channel=Leg],[drag-channel=ClassItem]').children('.item-tag').remove();
 });
--- spec/javascripts/dimTagRemover.spec.js
+++ spec/javascripts/dimTagRemover.spec.js
@@ -1,11 +1,12 @@
 describe('dimTagRemover.js', function() {
 
-  require('dimTagRemover.js');
-  const postal = require('postal');
+  const fateBus = require('fateBus.js');
+  const brunchModule = {id:'test'+this.result.description};
 
   describe('when a refresh occurs', function() {
 
     beforeEach(function() {
+      fateBus.registerModule(brunchModule);
       loadFixtures(
         'kineticWeapon.html',
         'energyWeapon.html',
@@ -21,13 +22,13 @@ describe('dimTagRemover.js', function() {
 
     it('should remove all weapon and armor tags', function() {
       expect($('[drag-channel=Kinetic],[drag-channel=Energy],[drag-channel=Power],[drag-channel=Helmet],[drag-channel=Gauntlets],[drag-channel=Chest],[drag-channel=Leg],[drag-channel=ClassItem]').children('.item-tag').length).toBe(8);
-      postal.publish({topic:'fate.refresh'});
+      fateBus.publish(brunchModule, 'fate.refresh');
       expect($('[drag-channel=Kinetic],[drag-channel=Energy],[drag-channel=Power],[drag-channel=Helmet],[drag-channel=Gauntlets],[drag-channel=Chest],[drag-channel=Leg],[drag-channel=ClassItem]').children('.item-tag').length).toBe(0);
     });
 
     it('should leave other tags alone', function() {
       expect($('[drag-channel=Ghost]').children('.item-tag').length).toBe(1);
-      postal.publish({topic:'fate.refresh'});
+      fateBus.publish(brunchModule, 'fate.refresh');
       expect($('[drag-channel=Ghost]').children('.item-tag').length).toBe(1);
     });
   });
