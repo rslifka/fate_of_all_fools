bedd7ff51f3d91b7b6ca3c40edabb9375e937c92
Robert Slifka <robert.slifka@gmail.com>
2017-12-22 16:50:45 -0800

    Decorate weapons with rarity and type to prep for infusion calculation

--- app/weaponDecorator.js
+++ app/weaponDecorator.js
@@ -1,4 +1,5 @@
 const $ = require('jquery');
+const weaponDatabase = require('weaponDatabase.js');
 
 function storeWeaponNames() {
   $('[drag-channel=Kinetic],[drag-channel=Energy],[drag-channel=Power]').not('[data-fate-weapon-name]').each(function(index,element) {
@@ -6,6 +7,26 @@ function storeWeaponNames() {
   });
 }
 
+function storeWeaponRarity() {
+  $('[data-fate-weapon-name]').not('data-fate-weapon-rarity').each(function(index,element) {
+    const weaponName = $(this).attr('data-fate-weapon-name');
+    if (weaponDatabase.contains(weaponName)) {
+      $(this).attr('data-fate-weapon-rarity', weaponDatabase.get(weaponName).rarity);
+    }
+  });
+}
+
+function storeWeaponType() {
+  $('[data-fate-weapon-name]').not('data-fate-weapon-type').each(function(index,element) {
+    const weaponName = $(this).attr('data-fate-weapon-name');
+    if (weaponDatabase.contains(weaponName)) {
+      $(this).attr('data-fate-weapon-type', weaponDatabase.get(weaponName).type);
+    }
+  });
+}
+
 fateBus.subscribe(module, 'fate.refresh', function() {
   storeWeaponNames();
+  storeWeaponRarity();
+  storeWeaponType();
 });
--- spec/javascripts/weaponDecorator.spec.js
+++ spec/javascripts/weaponDecorator.spec.js
@@ -2,6 +2,7 @@ describe('weaponDecorator.js', function() {
 
   const fateBus = require('fateBus.js');
   const brunchModule = {id:'test'+this.result.description};
+  const weaponDatabase = require('weaponDatabase');
 
   beforeEach(function() {
     fateBus.registerModule(brunchModule);
@@ -15,6 +16,19 @@ describe('weaponDecorator.js', function() {
         'energyWeaponRaw.html',
         'powerWeaponRaw.html'
       );
+
+      spyOn(weaponDatabase, 'contains').and.returnValue(true);
+      spyOn(weaponDatabase, 'get').and.callFake(function(weaponName) {
+        switch(weaponName) {
+          case 'Origin Story':
+            return {name: 'Origin Story', rarity: 'legendary', type: 'Auto Rifle'};
+          case 'Annual Skate':
+            return {name: 'Annual Skate', rarity: 'legendary', type: 'Hand Cannon'};
+          case 'Alone as a god':
+            return {name: 'Alone as a god', rarity: 'legendary', type: 'Sniper Rifle'};
+        }
+      });
+
       fateBus.publish(brunchModule, 'fate.refresh');
     });
 
@@ -22,6 +36,18 @@ describe('weaponDecorator.js', function() {
       expect($('[drag-channel=Kinetic]')).toHaveAttr('data-fate-weapon-name', 'Origin Story');
       expect($('[drag-channel=Energy]')).toHaveAttr('data-fate-weapon-name', 'Annual Skate');
       expect($('[drag-channel=Power]')).toHaveAttr('data-fate-weapon-name', 'Alone as a god');
+    });
+
+    it('should store the type of the weapon', function() {
+      expect($('[drag-channel=Kinetic]')).toHaveAttr('data-fate-weapon-type', 'Auto Rifle');
+      expect($('[drag-channel=Energy]')).toHaveAttr('data-fate-weapon-type', 'Hand Cannon');
+      expect($('[drag-channel=Power]')).toHaveAttr('data-fate-weapon-type', 'Sniper Rifle');
+    });
+
+    it('should store the rarity of the weapon', function() {
+      expect($('[drag-channel=Kinetic]')).toHaveAttr('data-fate-weapon-rarity', 'legendary');
+      expect($('[drag-channel=Energy]')).toHaveAttr('data-fate-weapon-rarity', 'legendary');
+      expect($('[drag-channel=Power]')).toHaveAttr('data-fate-weapon-rarity', 'legendary');
     });
 
     it('should not overwrite the original weapon name on further refreshes', function() {
