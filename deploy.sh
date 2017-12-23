53a1e02cf3d72ce28bf64e57a384be6718fb86b2
Robert Slifka <robert.slifka@gmail.com>
2017-12-23 00:39:40 -0800

    Update commits to make it clear where they originate

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 setup_git() {
-  git config --global user.email "travis+fateOfAllFools@travis-ci.org"
-  git config --global user.name "Travis CI (FATE)"
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis CI"
   git config credential.helper "store --file=.git/fate_credentials"
   echo "https://${GH_TOKEN}:@github.com" > .git/fate_credentials
   git remote add origin https://github.com/rslifka/fate_of_all_fools.git > /dev/null 2>&1
@@ -12,7 +12,7 @@ commit_artifacts() {
   git add docs/fateOfAllFools.css
   git add docs/fateOfAllFools.js
   git add fateOfAllFools.user.js
-  git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+  git commit --message "AUTO-CREATED Release Artifact v$TRAVIS_BUILD_NUMBER c/o Travis"
   git push --quiet --set-upstream origin master
 }
 
