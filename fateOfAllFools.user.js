401d0f28b5e054e0865231391cb41fe597e6b4f7
Travis CI (FATE) <travis+fateOfAllFools@travis-ci.org>
2017-12-23 08:35:12 +0000

    Travis build: 23

---
+++ fateOfAllFools.user.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @author      Robert Slifka (github @rslifka)
+// @connect     docs.google.com
+// @connect     rslifka.github.io
+// @copyright   2017-2018, Robert Slifka (https://github.com/rslifka/fate_of_all_fools)
+// @description Enhancements to the Destiny Item Manager
+// @grant       GM_addStyle
+// @grant       GM_getResourceText
+// @grant       GM_getValue
+// @grant       GM_log
+// @grant       GM_setValue
+// @grant       GM_xmlhttpRequest
+// @homepage    https://github.com/rslifka/fate_of_all_fools
+// @license     MIT; https://raw.githubusercontent.com/rslifka/fate_of_all_fools/master/LICENSE.txt
+// @copyright   2017, Robert Slifka (https://github.com/rslifka/fate_of_all_fools)
+// @match       https://*.destinyitemmanager.com/*
+// @name        Fate of All Fools
+// @require     https://rslifka.github.io/fate_of_all_fools/fateOfAllFools.js
+// @resource    fateOfAllFoolsCSS https://rslifka.github.io/fate_of_all_fools/fateOfAllFools.css
+// @run-at      document-start
+// @supportURL  https://github.com/rslifka/fate_of_all_fools/issues
+// @version     23
+// ==/UserScript==
