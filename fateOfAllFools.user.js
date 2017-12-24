f5fe270816c4f259f3f21ef27c5f796fc0e515dd
Travis CI <travis@travis-ci.org>
2017-12-24 02:08:34 +0000

    AUTO-CREATED Release Artifact v269 c/o Travis

--- fateOfAllFools.user.js
+++ fateOfAllFools.user.js
@@ -18,5 +18,5 @@
 // @resource    fateOfAllFoolsCSS https://rslifka.github.io/fate_of_all_fools/fateOfAllFools.css
 // @run-at      document-start
 // @supportURL  https://github.com/rslifka/fate_of_all_fools/issues
-// @version     268
+// @version     269
 // ==/UserScript==
