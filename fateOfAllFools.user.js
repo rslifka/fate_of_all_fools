1a67ff9c0c5c0c8e69bd7b83b61e3fa69d426bce
Travis CI <travis@travis-ci.org>
2017-12-24 16:43:10 +0000

    AUTO-CREATED Release Artifact v589 c/o Travis

--- fateOfAllFools.user.js
+++ fateOfAllFools.user.js
@@ -19,5 +19,5 @@
 // @run-at      document-start
 // @supportURL  https://github.com/rslifka/fate_of_all_fools/issues
 // @updateURL   https://openuserjs.org/meta/rslifka/FateOfAllFools_-_DIM_Customization.meta.js
-// @version     588
+// @version     589
 // ==/UserScript==
