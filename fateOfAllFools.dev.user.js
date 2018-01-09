34b8cd23e9f328c1085471b5b1414673d0316589
Robert Slifka <robert.slifka@gmail.com>
2018-01-08 22:05:01 -0800

    Include only what's necessary to test locally

--- fateOfAllFools.dev.user.js
+++ fateOfAllFools.dev.user.js
@@ -1,21 +1,15 @@
 // ==UserScript==
-// @author      Robert Slifka (GitHub @rslifka)
 // @connect     docs.google.com
 // @connect     rslifka.github.io
-// @copyright   2017-2018, Robert Slifka (https://github.com/rslifka/fate_of_all_fools)
-// @description (DEVELOPMENT) Enhancements to the Destiny Item Manager
 // @grant       GM_addStyle
 // @grant       GM_getResourceText
 // @grant       GM_getValue
 // @grant       GM_log
 // @grant       GM_setValue
 // @grant       GM_xmlhttpRequest
-// @homepage    https://github.com/rslifka/fate_of_all_fools
-// @license     MIT; https://raw.githubusercontent.com/rslifka/fate_of_all_fools/master/LICENSE.txt
 // @match       https://*.destinyitemmanager.com/*
 // @name        (DEVELOPMENT) Fate of All Fools
 // @require     file:///Users/rslifka/Dropbox/workspace/fate_of_all_fools/public/fateOfAllFools.js
 // @resource    fateOfAllFoolsCSS file:///Users/rslifka/Dropbox/workspace/fate_of_all_fools/public/fateOfAllFools.css
 // @run-at      document-idle
-// @supportURL  https://github.com/rslifka/fate_of_all_fools/issues
 // ==/UserScript==
