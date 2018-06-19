1da950526a4e7b5e244f170791ed9e6f2f9456c9
Robert Slifka <robert.slifka@gmail.com>
2018-06-19 05:24:35 -0700

    Begin store the name and serial number in preparation for Forsaken

--- app/armorDecorator.js
+++ app/armorDecorator.js
@@ -1,20 +1,34 @@
 const $ = require('jquery');
 
-function storeModStatus() {
-  $('[drag-channel=Helmet],[drag-channel=Gauntlets],[drag-channel=Chest],[drag-channel=Leg],[drag-channel=ClassItem]').each(function(index,element) {
-    $(this).attr('data-fate-is-modded', $(this).children('.item-img.complete').length > 0);
+function storeArmorName() {
+  $('[drag-channel=Helmet],[drag-channel=Gauntlets],[drag-channel=Chest],[drag-channel=Leg],[drag-channel=ClassItem]').not('[data-fate-armor-name]').each(function(index,element) {
+    $(this).attr('data-fate-armor-name', $(this).attr('title').split("\n")[0]);
   });
 }
 
 function storeBaseLightLevel() {
-  $('[drag-channel=Helmet],[drag-channel=Gauntlets],[drag-channel=Chest],[drag-channel=Leg],[drag-channel=ClassItem]').each(function(index,element) {
+  $('[drag-channel=Helmet],[drag-channel=Gauntlets],[drag-channel=Chest],[drag-channel=Leg],[drag-channel=ClassItem]').not('[data-fate-base-light]').each(function(index,element) {
      const itemStatValue = parseInt($(this).children('.item-stat').text().match(/(\d+)/));
      const baseLightLevel = ($(this).attr('data-fate-is-modded') === 'true') ? itemStatValue-5 : itemStatValue;
      $(this).attr('data-fate-base-light', baseLightLevel);
   });
 }
 
+function storeModStatus() {
+  $('[drag-channel=Helmet],[drag-channel=Gauntlets],[drag-channel=Chest],[drag-channel=Leg],[drag-channel=ClassItem]').each(function(index,element) {
+    $(this).attr('data-fate-is-modded', $(this).children('.item-img.complete').length > 0);
+  });
+}
+
+function storeSerialNumber() {
+  $('[drag-channel=Helmet],[drag-channel=Gauntlets],[drag-channel=Chest],[drag-channel=Leg],[drag-channel=ClassItem]').not('[data-fate-serial]').each(function(index,element) {
+    $(this).attr('data-fate-serial', $(this).attr('id').split("-")[0])
+  });
+}
+
 fateBus.subscribe(module, 'fate.refresh', function() {
+  storeArmorName();
   storeModStatus();
   storeBaseLightLevel();
+  storeSerialNumber();
 });
--- spec/javascripts/armorDecorator.spec.js
+++ spec/javascripts/armorDecorator.spec.js
@@ -15,35 +15,83 @@ describe('armorDecorator.js', function() {
         'armorPreDecoration/classShadowsMark335.html',
         'armorPreDecoration/gauntletsIronTruageGauntlets335.html',
         'armorPreDecoration/legWinterhartGreaves329.html',
-        'armorPreDecoration/helmetKheprisHorn335.html',
-        'armorPreDecoration/helmetLegionBane329.html',
         'armorPreDecoration/helmetWildwoodHelm333.html',
       );
       fateBus.publish(brunchModule, 'fate.refresh');
     });
 
-    describe('light decoration', function() {
-      it('should record its base light', function() {
-        expect('[title*="Horn"]').toHaveAttr('data-fate-base-light', '330');
-        expect('[title*="Wildwood"]').toHaveAttr('data-fate-base-light', '328');
-        expect('[title*="Bane"]').toHaveAttr('data-fate-base-light', '329');
-        expect('[title*="Noble"]').toHaveAttr('data-fate-base-light', '330');
-        expect('[title*="Winterhart"]').toHaveAttr('data-fate-base-light', '329');
-        expect('[title*="Shadow"]').toHaveAttr('data-fate-base-light', '330');
-        expect('[title*="Truage"]').toHaveAttr('data-fate-base-light', '330');
-      });
-    })
+    it('should store the original armor name', function() {
+      expect('[title*="Wildwood"]').toHaveAttr('data-fate-armor-name', 'Wildwood Helm');
+      expect('[title*="Noble"]').toHaveAttr('data-fate-armor-name', 'Noble Constant Type 2');
+      expect('[title*="Winterhart"]').toHaveAttr('data-fate-armor-name', 'Winterhart Greaves');
+      expect('[title*="Shadow"]').toHaveAttr('data-fate-armor-name', "Shadow's Mark");
+      expect('[title*="Truage"]').toHaveAttr('data-fate-armor-name', 'Iron Truage Gauntlets');
+    });
 
-    describe('mod decoration', function() {
-      it('should record whether or not its modded', function() {
-        expect('[title*="Horn"]').toHaveAttr('data-fate-is-modded', 'true');
-        expect('[title*="Wildwood"]').toHaveAttr('data-fate-is-modded', 'true');
-        expect('[title*="Bane"]').toHaveAttr('data-fate-is-modded', 'false');
-        expect('[title*="Noble"]').toHaveAttr('data-fate-is-modded', 'false');
-        expect('[title*="Winterhart"]').toHaveAttr('data-fate-is-modded', 'false');
-        expect('[title*="Shadow"]').toHaveAttr('data-fate-is-modded', 'true');
-        expect('[title*="Truage"]').toHaveAttr('data-fate-is-modded', 'true');
+    it('should record its base light', function() {
+      expect('[title*="Wildwood"]').toHaveAttr('data-fate-base-light', '328');
+      expect('[title*="Noble"]').toHaveAttr('data-fate-base-light', '330');
+      expect('[title*="Winterhart"]').toHaveAttr('data-fate-base-light', '329');
+      expect('[title*="Shadow"]').toHaveAttr('data-fate-base-light', '330');
+      expect('[title*="Truage"]').toHaveAttr('data-fate-base-light', '330');
+    });
+
+    it('should record whether or not its modded', function() {
+      expect('[title*="Wildwood"]').toHaveAttr('data-fate-is-modded', 'true');
+      expect('[title*="Noble"]').toHaveAttr('data-fate-is-modded', 'false');
+      expect('[title*="Winterhart"]').toHaveAttr('data-fate-is-modded', 'false');
+      expect('[title*="Shadow"]').toHaveAttr('data-fate-is-modded', 'true');
+      expect('[title*="Truage"]').toHaveAttr('data-fate-is-modded', 'true');
+    });
+
+    it('shoud record the serial number', function() {
+      expect($('[drag-channel=Helmet]')).toHaveAttr('data-fate-serial', '6917529039249138892');
+      expect($('[drag-channel=Chest]')).toHaveAttr('data-fate-serial', '6917529034267773023');
+      expect($('[drag-channel=Leg]')).toHaveAttr('data-fate-serial', '6917529048121131444');
+      expect($('[drag-channel=ClassItem]')).toHaveAttr('data-fate-serial', '6917529032743550478');
+      expect($('[drag-channel=Gauntlets]')).toHaveAttr('data-fate-serial', '6917529044428519818');
+    });
+
+    describe('on subsequent refreshes', function() {
+
+      const CHANNELS = ['Helmet','Gauntlets','Chest','Leg','ClassItem'];
+
+      it('should not overwrite the original name', function() {
+        CHANNELS.forEach(function(channelName) {
+          $('[drag-channel='+channelName+']').attr('title', '_');
+        });
+        fateBus.publish(brunchModule, 'fate.refresh');
+        expect($('[drag-channel=Helmet]')).toHaveAttr('data-fate-armor-name', 'Wildwood Helm');
+        expect($('[drag-channel=Chest]')).toHaveAttr('data-fate-armor-name', 'Noble Constant Type 2');
+        expect($('[drag-channel=Leg]')).toHaveAttr('data-fate-armor-name', 'Winterhart Greaves');
+        expect($('[drag-channel=ClassItem]')).toHaveAttr('data-fate-armor-name', "Shadow's Mark");
+        expect($('[drag-channel=Gauntlets]')).toHaveAttr('data-fate-armor-name', 'Iron Truage Gauntlets');
       });
+
+      it('should not overwrite the base light', function() {
+        CHANNELS.forEach(function(channelName) {
+          $('[drag-channel='+channelName+']').children('.item-stat').text('400');
+        });
+        fateBus.publish(brunchModule, 'fate.refresh');
+        expect($('[drag-channel=Helmet]')).toHaveAttr('data-fate-base-light', '328');
+        expect($('[drag-channel=Chest]')).toHaveAttr('data-fate-base-light', '330');
+        expect($('[drag-channel=Leg]')).toHaveAttr('data-fate-base-light', '329');
+        expect($('[drag-channel=ClassItem]')).toHaveAttr('data-fate-base-light', '330');
+        expect($('[drag-channel=Gauntlets]')).toHaveAttr('data-fate-base-light', '330');
+      });
+
+      it('should not overwrite the serial number', function() {
+        CHANNELS.forEach(function(channelName) {
+          $('[drag-channel='+channelName+']').attr('id','_');
+        });
+        fateBus.publish(brunchModule, 'fate.refresh');
+        expect($('[drag-channel=Helmet]')).toHaveAttr('data-fate-serial', '6917529039249138892');
+        expect($('[drag-channel=Chest]')).toHaveAttr('data-fate-serial', '6917529034267773023');
+        expect($('[drag-channel=Leg]')).toHaveAttr('data-fate-serial', '6917529048121131444');
+        expect($('[drag-channel=ClassItem]')).toHaveAttr('data-fate-serial', '6917529032743550478');
+        expect($('[drag-channel=Gauntlets]')).toHaveAttr('data-fate-serial', '6917529044428519818');
+      });
+
     });
 
   });
