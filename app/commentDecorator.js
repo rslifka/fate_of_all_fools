8d79d5ca45578f29ad4140a1dd9ff55733328958
Robert Slifka <robert.slifka@gmail.com>
2018-06-24 22:28:48 -0700

    Generate comments from attrs to reduce reliance on database

--- app/commentDecorator.js
+++ app/commentDecorator.js
@@ -1,19 +1,13 @@
 const $ = require('jquery');
 const logger = require('logger');
-const weaponDatabase = require('weaponDatabase.js');
 
 function updateTitles() {
   logger.log('commentDecorator.js: Updating weapon tooltips');
-  $('[data-fate-weapon-name]').each(function(index,element) {
-    const weaponName = $(this).attr('data-fate-weapon-name');
-    if (!weaponDatabase.contains(weaponName)) {
-      return true;
-    }
-    const weapon = weaponDatabase.get(weaponName);
-    $(this).attr(
-      'title',
-      weaponName + ' // ' + weapon.type + '\n' + weapon.comments
-    );
+  $('[data-fate-weapon-registered=true]').each(function(index,element) {
+    const name = $(this).attr('data-fate-weapon-name');
+    const type = $(this).attr('data-fate-weapon-type');
+    const comments = $(this).attr('data-fate-comment');
+    $(this).attr('title',name + ' // ' + type + '\n' + comments);
   });
 }
 
--- spec/javascripts/commentDecorator.spec.js
+++ spec/javascripts/commentDecorator.spec.js
@@ -3,32 +3,29 @@ describe('commentDecorator.js', function() {
   const fateBus = require('fateBus.js');
   const brunchModule = {id:'test'+this.result.description};
 
-  const weaponDatabase = require('weaponDatabase');
+  beforeEach(function() {
+    fateBus.registerModule(brunchModule);
+  });
 
   describe('in response to fate.refresh', function() {
 
-    it('should replace the DIM tooltip with our comment', function() {
+    beforeEach(function() {
       loadFixtures(
         'kineticWeapon.html',
         'energyWeapon.html',
         'powerWeapon.html'
       );
+      $('[data-fate-weapon-name]').attr('data-fate-weapon-registered', true);
+      $('[data-fate-weapon-name="Origin Story"]').attr('data-fate-comment', 'Rampage makes this great for lots of adds killing');
+      $('[data-fate-weapon-name="Origin Story"]').attr('data-fate-weapon-type', 'Auto Rifle');
+      $('[data-fate-weapon-name="Annual Skate"]').attr('data-fate-comment', 'Seems strictly worse than the Minuet?');
+      $('[data-fate-weapon-name="Annual Skate"]').attr('data-fate-weapon-type', 'Hand Cannon');
+      $('[data-fate-weapon-name="Alone as a god"]').attr('data-fate-comment', 'TT, fast ADS, fast reload, awesome');
+      $('[data-fate-weapon-name="Alone as a god"]').attr('data-fate-weapon-type', 'Sniper Rifle');
+    });
 
-      spyOn(weaponDatabase, 'contains').and.returnValue(true);
-      spyOn(weaponDatabase, 'get').and.callFake(function(weaponName) {
-        switch(weaponName) {
-          case 'Origin Story':
-            return {name: 'Origin Story', type: 'Auto Rifle', comments: 'Rampage makes this great for lots of adds killing'};
-          case 'Annual Skate':
-            return {name: 'Annual Skate', type: 'Hand Cannon', comments: 'Seems strictly worse than the Minuet?'};
-          case 'Alone as a god':
-            return {name: 'Alone as a god', type: 'Sniper Rifle', comments: 'TT, fast ADS, fast reload, awesome'};
-        }
-      });
-
-      fateBus.registerModule(brunchModule);
+    it('should replace the DIM tooltip with our comment', function() {
       fateBus.publish(brunchModule, 'fate.refresh');
-
       expect($('[data-fate-weapon-name="Origin Story"]')).toHaveAttr('title', "Origin Story // Auto Rifle\nRampage makes this great for lots of adds killing");
       expect($('[data-fate-weapon-name="Annual Skate"]')).toHaveAttr('title', "Annual Skate // Hand Cannon\nSeems strictly worse than the Minuet?");
       expect($('[data-fate-weapon-name="Alone as a god"]')).toHaveAttr('title', "Alone as a god // Sniper Rifle\nTT, fast ADS, fast reload, awesome");
