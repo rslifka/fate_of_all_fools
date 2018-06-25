061c79c86dec927d07c9566f982c9909abfbccad
Robert Slifka <robert.slifka@gmail.com>
2018-06-25 13:42:32 -0700

    Use the right TSV data source

--- app/rollDataRefresher.js
+++ app/rollDataRefresher.js
@@ -1,5 +1,5 @@
 const i = require('itemDataRefresher.js');
 
 fateBus.subscribe(module, 'fate.configurationLoaded', function(topic, configuration) {
-  new i.ItemDataRefresher('roll', configuration.shaderDataTSV)
+  new i.ItemDataRefresher('roll', configuration.rollDataTSV)
 });
