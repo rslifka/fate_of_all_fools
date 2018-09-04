23ad528a85377d2f620307366dde072c4cf37f49
Robert Slifka <robert.slifka@gmail.com>
2018-09-03 18:56:42 -0700

    Disable infusion fodder calculations until it can be fixed. Better not there than incorrect

--- app/fodderIndicator.js
+++ app/fodderIndicator.js
@@ -1,113 +1,113 @@
-const $ = require('jquery');
-const logger = require('logger.js');
-
-function prepareFodderIndicator() {
-  $('[data-fate-weapon-registered="true"]').each(function(index,element) {
-    if ($(this).children('.fate-fodder.fate-glyph.fglyph-up').length > 0) {
-      return;
-    }
-    $(this).append($('<div>', {'class': 'fate-fodder fate-middling fate-glyph fglyph-up', 'style':'display:none'}));
-  });
-}
-
-function calculateFodder() {
-  const goodWeaponsToInfuse = new Map();
-  $('[data-fate-weapon-registered="true"]').each(function(index,element) {
-    if ($(this).attr('data-fate-weapon-junk') === 'true') {
-      return;
-    }
-    if (!$(this).is('[data-fate-weapon-rarity=legendary],[data-fate-weapon-rarity=exotic]')) {
-      return;
-    }
-    const weaponName = $(this).attr('data-fate-weapon-name');
-    const weaponType = $(this).attr('data-fate-weapon-type');
-    const weaponLight = $(this).attr('data-fate-base-light');
-    const weaponData = {
-      type: weaponType,
-      light: parseInt(weaponLight),
-      domElement: this,
-      toString: function() {return weaponType + ' - ' + weaponName + ' ('+weaponLight+')';}
-    };
-    if (goodWeaponsToInfuse.has(weaponType)) {
-      goodWeaponsToInfuse.set(weaponType, goodWeaponsToInfuse.get(weaponType).concat(weaponData));
-    } else {
-      goodWeaponsToInfuse.set(weaponType, [weaponData]);
-    }
-  });
-  return goodWeaponsToInfuse;
-}
-
-function styleFodderIndicators(goodWeaponsToBoost) {
-  $('.fate-fodder').hide();
-  $('.fate-fodder').removeClass('fate-left-bump');
-  $('.fate-fodder').parent().each(function(index,element) {
-    const isJunk = $(this).attr('data-fate-weapon-junk') === 'true';
-    const isDupe = $(this).attr('data-fate-weapon-dupe') === 'true';
-    if (!(isJunk || isDupe)) {
-      return;
-    }
-
-    const weaponType = $(this).attr('data-fate-weapon-type');
-    const boostableWeapons = goodWeaponsToBoost.get(weaponType);
-    if (boostableWeapons === undefined) {
-      return;
-    }
-    const light = parseInt($(this).attr('data-fate-base-light'));
-    if ((boostableWeapons.filter(w => w.light < light)).length > 0) {
-      if ($(this).children('.fate-infusion:visible').length > 0) {
-        $(this).children('.fate-fodder').addClass('fate-left-bump');
-      }
-      $(this).children('.fate-fodder').show();
-    }
-  });
-}
-
-function onMouseEnter() {
-  $('[drag-channel=Kinetic],[drag-channel=Energy],[drag-channel=Power]').addClass('fate-search-hidden');
-  $(this).parent().removeClass('fate-search-hidden');
-
-  const weaponType = $(this).parent().attr('data-fate-weapon-type');
-  const weaponLight = $(this).parent().attr('data-fate-base-light');
-
-  $('[data-fate-weapon-type="'+weaponType+'"]').each(function(index,element) {
-    if ($(this).attr('data-fate-weapon-junk') === 'true') {
-      return;
-    }
-    if (!$(this).is('[data-fate-weapon-rarity=legendary],[data-fate-weapon-rarity=exotic]')) {
-      return;
-    }
-    const lowerLight = parseInt($(this).attr('data-fate-base-light')) < weaponLight;
-    if (lowerLight) {
-      $(this).removeClass('fate-search-hidden');
-    }
-  });
-}
-
-function onMouseLeave() {
-  $('.fate-search-hidden').removeClass('fate-search-hidden');
-}
-
-function registerListeners() {
-  $('.fate-fodder:visible').on('mouseenter.fodder', onMouseEnter);
-  $('.fate-fodder:visible').on('mouseleave.fodder', onMouseLeave);
-}
-
-fateBus.subscribe(module, 'fate.infusionCalculated', function() {
-  logger.log('fodderIndicator.js: Calculating fodder');
-  prepareFodderIndicator();
-  styleFodderIndicators(calculateFodder());
-  registerListeners();
-});
-
-/*
-  jasmine-jquery doesn't seem to play well these days. Not sure why but it can't
-  seem to trigger events via $.trigger, so we're going to use our bus to test the
-  events.
-*/
-fateBus.subscribe(module, 'fate.test.mouseenter.fodder', function(msg, selector) {
-  $(selector).each(onMouseEnter);
-});
-
-fateBus.subscribe(module, 'fate.test.mouseleave.fodder', function(msg, selector) {
-  $(selector).each(onMouseLeave);
-});
+// const $ = require('jquery');
+// const logger = require('logger.js');
+//
+// function prepareFodderIndicator() {
+//   $('[data-fate-weapon-registered="true"]').each(function(index,element) {
+//     if ($(this).children('.fate-fodder.fate-glyph.fglyph-up').length > 0) {
+//       return;
+//     }
+//     $(this).append($('<div>', {'class': 'fate-fodder fate-middling fate-glyph fglyph-up', 'style':'display:none'}));
+//   });
+// }
+//
+// function calculateFodder() {
+//   const goodWeaponsToInfuse = new Map();
+//   $('[data-fate-weapon-registered="true"]').each(function(index,element) {
+//     if ($(this).attr('data-fate-weapon-junk') === 'true') {
+//       return;
+//     }
+//     if (!$(this).is('[data-fate-weapon-rarity=legendary],[data-fate-weapon-rarity=exotic]')) {
+//       return;
+//     }
+//     const weaponName = $(this).attr('data-fate-weapon-name');
+//     const weaponType = $(this).attr('data-fate-weapon-type');
+//     const weaponLight = $(this).attr('data-fate-base-light');
+//     const weaponData = {
+//       type: weaponType,
+//       light: parseInt(weaponLight),
+//       domElement: this,
+//       toString: function() {return weaponType + ' - ' + weaponName + ' ('+weaponLight+')';}
+//     };
+//     if (goodWeaponsToInfuse.has(weaponType)) {
+//       goodWeaponsToInfuse.set(weaponType, goodWeaponsToInfuse.get(weaponType).concat(weaponData));
+//     } else {
+//       goodWeaponsToInfuse.set(weaponType, [weaponData]);
+//     }
+//   });
+//   return goodWeaponsToInfuse;
+// }
+//
+// function styleFodderIndicators(goodWeaponsToBoost) {
+//   $('.fate-fodder').hide();
+//   $('.fate-fodder').removeClass('fate-left-bump');
+//   $('.fate-fodder').parent().each(function(index,element) {
+//     const isJunk = $(this).attr('data-fate-weapon-junk') === 'true';
+//     const isDupe = $(this).attr('data-fate-weapon-dupe') === 'true';
+//     if (!(isJunk || isDupe)) {
+//       return;
+//     }
+//
+//     const weaponType = $(this).attr('data-fate-weapon-type');
+//     const boostableWeapons = goodWeaponsToBoost.get(weaponType);
+//     if (boostableWeapons === undefined) {
+//       return;
+//     }
+//     const light = parseInt($(this).attr('data-fate-base-light'));
+//     if ((boostableWeapons.filter(w => w.light < light)).length > 0) {
+//       if ($(this).children('.fate-infusion:visible').length > 0) {
+//         $(this).children('.fate-fodder').addClass('fate-left-bump');
+//       }
+//       $(this).children('.fate-fodder').show();
+//     }
+//   });
+// }
+//
+// function onMouseEnter() {
+//   $('[drag-channel=Kinetic],[drag-channel=Energy],[drag-channel=Power]').addClass('fate-search-hidden');
+//   $(this).parent().removeClass('fate-search-hidden');
+//
+//   const weaponType = $(this).parent().attr('data-fate-weapon-type');
+//   const weaponLight = $(this).parent().attr('data-fate-base-light');
+//
+//   $('[data-fate-weapon-type="'+weaponType+'"]').each(function(index,element) {
+//     if ($(this).attr('data-fate-weapon-junk') === 'true') {
+//       return;
+//     }
+//     if (!$(this).is('[data-fate-weapon-rarity=legendary],[data-fate-weapon-rarity=exotic]')) {
+//       return;
+//     }
+//     const lowerLight = parseInt($(this).attr('data-fate-base-light')) < weaponLight;
+//     if (lowerLight) {
+//       $(this).removeClass('fate-search-hidden');
+//     }
+//   });
+// }
+//
+// function onMouseLeave() {
+//   $('.fate-search-hidden').removeClass('fate-search-hidden');
+// }
+//
+// function registerListeners() {
+//   $('.fate-fodder:visible').on('mouseenter.fodder', onMouseEnter);
+//   $('.fate-fodder:visible').on('mouseleave.fodder', onMouseLeave);
+// }
+//
+// fateBus.subscribe(module, 'fate.infusionCalculated', function() {
+//   logger.log('fodderIndicator.js: Calculating fodder');
+//   prepareFodderIndicator();
+//   styleFodderIndicators(calculateFodder());
+//   registerListeners();
+// });
+//
+// /*
+//   jasmine-jquery doesn't seem to play well these days. Not sure why but it can't
+//   seem to trigger events via $.trigger, so we're going to use our bus to test the
+//   events.
+// */
+// fateBus.subscribe(module, 'fate.test.mouseenter.fodder', function(msg, selector) {
+//   $(selector).each(onMouseEnter);
+// });
+//
+// fateBus.subscribe(module, 'fate.test.mouseleave.fodder', function(msg, selector) {
+//   $(selector).each(onMouseLeave);
+// });
--- spec/javascripts/fodderIndicator.spec.js
+++ spec/javascripts/fodderIndicator.spec.js
@@ -1,132 +1,132 @@
-describe('fodderIndicator.js', function() {
-
-  const fateBus = require('fateBus.js');
-  const brunchModule = {id:'test'+this.result.description};
-
-  beforeEach(function() {
-    fateBus.registerModule(brunchModule);
-  });
-
-  describe('in response to a fate.infusionCalculated event', function() {
-
-    describe('when this weapon is not registered', function() {
-      beforeEach(function() {
-        loadFixtures(
-          'infusion/energyPerseverance300.html', // Auto - Legendary
-          'infusion/kineticOriginStory306Unregistered.html', // Auto - Legendary
-        );
-      });
-      it('should not get a fusion indicator', function() {
-        fateBus.publish(brunchModule, 'fate.infusionCalculated');
-        expect($('[data-fate-weapon-name="Origin Story"] .fate-fodder.fate-middling.fate-glyph.fglyph-up')).not.toExist();
-      });
-    });
-
-    describe('when there are no other weapons of the same type', function() {
-      beforeEach(function() {
-        loadFixtures(
-          'infusion/kineticOriginStory306.html', // Auto
-          'infusion/powerAloneAsAGod305.html',   // Sniper
-        );
-      });
-      it('should not get a fusion indicator', function() {
-        fateBus.publish(brunchModule, 'fate.infusionCalculated');
-        expect($('[data-fate-weapon-name="Origin Story"] .fate-fodder.fate-middling.fate-glyph.fglyph-up')).toBeHidden();
-      });
-    });
-
-    describe('when I am a higher light weapon', function() {
-
-      describe('when there are only junk, uncommon or rare weapons at lower light', function() {
-        beforeEach(function() {
-          loadFixtures(
-            'infusion/kineticSurosThrowback200.html', // Auto - Uncommon
-            'infusion/energySolemnHymn300.html',      // Auto - Junk
-            'infusion/kineticCuboidARu303.html',      // Auto - Rare
-            'infusion/kineticOriginStory306.html',    // Auto - Legendary
-          );
-        });
-        it('should not get a fodder indicator', function() {
-          fateBus.publish(brunchModule, 'fate.infusionCalculated');
-          expect($('[data-fate-weapon-name="Origin Story"] .fate-fodder.fate-middling.fate-glyph.fglyph-up')).toBeHidden();
-        });
-      });
-
-      describe('when there are legendary weapons at lower light', function() {
-        beforeEach(function() {
-          loadFixtures(
-            'infusion/energyPerseverance300.html', // Auto - Legendary
-            'infusion/kineticOriginStory306Junk.html', // Auto - Legendary
-            'infusion/kineticOriginStory306Junk.html', // Auto - Legendary
-          );
-        });
-        it('should get a fodder indicator', function() {
-          fateBus.publish(brunchModule, 'fate.infusionCalculated');
-          expect($('[data-fate-weapon-name="Origin Story"] .fate-fodder.fate-middling.fate-glyph.fglyph-up')).toBeVisible();
-        });
-      });
-
-      describe('when there are exotic weapons at lower light', function() {
-        beforeEach(function() {
-          loadFixtures(
-            'infusion/kineticSweetBusiness200.html', // Auto - Exotic
-            'infusion/kineticOriginStory306Dupe.html',   // Auto - Legendary
-            'infusion/kineticOriginStory306Dupe.html',   // Auto - Legendary
-          );
-        });
-        it('should get a fodder indicator', function() {
-          fateBus.publish(brunchModule, 'fate.infusionCalculated');
-          expect($('[data-fate-weapon-name="Origin Story"] .fate-fodder.fate-middling.fate-glyph.fglyph-up')).toBeVisible();
-        });
-      });
-
-      describe('when i am higher light, not junk and not a dupe', function() {
-        beforeEach(function() {
-          loadFixtures(
-            'infusion/energyPerseverance300.html', // Auto - Legendary
-            'infusion/kineticOriginStory306.html', // Auto - Legendary
-          );
-        });
-        it('should not get a fodder icon because i am the last of my kind', function() {
-          fateBus.publish(brunchModule, 'fate.infusionCalculated');
-          expect($('[data-fate-weapon-name="Origin Story"] .fate-fodder.fate-middling.fate-glyph.fglyph-up')).not.toBeVisible();
-        });
-      });
-
-      describe('mouse interaction', function() {
-
-        beforeEach(function() {
-          loadFixtures(
-            'infusion/kineticSurosThrowback200.html', // Auto - Uncommon
-            'infusion/kineticSweetBusiness200.html',  // Auto - Exotic
-            'infusion/energyPerseverance300.html',    // Auto - Legendary
-            'infusion/energySolemnHymn300.html',      // Auto - Junk
-            'infusion/kineticCuboidARu303.html',      // Auto - Rare
-            'infusion/kineticOriginStory306.html',    // Auto - Legendary
-          );
-        });
-
-        it('should show other items of the same type with lower light', function() {
-          fateBus.publish(brunchModule, 'fate.infusionCalculated');
-
-          fateBus.publish(brunchModule, 'fate.test.mouseenter.fodder', '[data-fate-weapon-name="Origin Story"] .fate-fodder');
-
-          expect($('[data-fate-weapon-name="Suros Throwback"]')).toHaveClass('fate-search-hidden');
-          expect($('[data-fate-weapon-name="Sweet Business"]')).not.toHaveClass('fate-search-hidden');
-          expect($('[data-fate-weapon-name="Perseverance"]')).not.toHaveClass('fate-search-hidden');
-          expect($('[data-fate-weapon-name="Solemn Hymn"]')).toHaveClass('fate-search-hidden');
-          expect($('[data-fate-weapon-name="Cuboid ARu"]')).toHaveClass('fate-search-hidden');
-          expect($('[data-fate-weapon-name="Origin Story"]')).not.toHaveClass('fate-search-hidden');
-
-          fateBus.publish(brunchModule, 'fate.test.mouseleave.fodder', '[data-fate-weapon-name="Origin Story"] .fate-fodder');
-
-          expect($('[data-fate-weapon-name]')).not.toHaveClass('fate-search-hidden');
-        });
-
-      });
-
-    });
-
-  });
-
-});
+// describe('fodderIndicator.js', function() {
+//
+//   const fateBus = require('fateBus.js');
+//   const brunchModule = {id:'test'+this.result.description};
+//
+//   beforeEach(function() {
+//     fateBus.registerModule(brunchModule);
+//   });
+//
+//   describe('in response to a fate.infusionCalculated event', function() {
+//
+//     describe('when this weapon is not registered', function() {
+//       beforeEach(function() {
+//         loadFixtures(
+//           'infusion/energyPerseverance300.html', // Auto - Legendary
+//           'infusion/kineticOriginStory306Unregistered.html', // Auto - Legendary
+//         );
+//       });
+//       it('should not get a fusion indicator', function() {
+//         fateBus.publish(brunchModule, 'fate.infusionCalculated');
+//         expect($('[data-fate-weapon-name="Origin Story"] .fate-fodder.fate-middling.fate-glyph.fglyph-up')).not.toExist();
+//       });
+//     });
+//
+//     describe('when there are no other weapons of the same type', function() {
+//       beforeEach(function() {
+//         loadFixtures(
+//           'infusion/kineticOriginStory306.html', // Auto
+//           'infusion/powerAloneAsAGod305.html',   // Sniper
+//         );
+//       });
+//       it('should not get a fusion indicator', function() {
+//         fateBus.publish(brunchModule, 'fate.infusionCalculated');
+//         expect($('[data-fate-weapon-name="Origin Story"] .fate-fodder.fate-middling.fate-glyph.fglyph-up')).toBeHidden();
+//       });
+//     });
+//
+//     describe('when I am a higher light weapon', function() {
+//
+//       describe('when there are only junk, uncommon or rare weapons at lower light', function() {
+//         beforeEach(function() {
+//           loadFixtures(
+//             'infusion/kineticSurosThrowback200.html', // Auto - Uncommon
+//             'infusion/energySolemnHymn300.html',      // Auto - Junk
+//             'infusion/kineticCuboidARu303.html',      // Auto - Rare
+//             'infusion/kineticOriginStory306.html',    // Auto - Legendary
+//           );
+//         });
+//         it('should not get a fodder indicator', function() {
+//           fateBus.publish(brunchModule, 'fate.infusionCalculated');
+//           expect($('[data-fate-weapon-name="Origin Story"] .fate-fodder.fate-middling.fate-glyph.fglyph-up')).toBeHidden();
+//         });
+//       });
+//
+//       describe('when there are legendary weapons at lower light', function() {
+//         beforeEach(function() {
+//           loadFixtures(
+//             'infusion/energyPerseverance300.html', // Auto - Legendary
+//             'infusion/kineticOriginStory306Junk.html', // Auto - Legendary
+//             'infusion/kineticOriginStory306Junk.html', // Auto - Legendary
+//           );
+//         });
+//         it('should get a fodder indicator', function() {
+//           fateBus.publish(brunchModule, 'fate.infusionCalculated');
+//           expect($('[data-fate-weapon-name="Origin Story"] .fate-fodder.fate-middling.fate-glyph.fglyph-up')).toBeVisible();
+//         });
+//       });
+//
+//       describe('when there are exotic weapons at lower light', function() {
+//         beforeEach(function() {
+//           loadFixtures(
+//             'infusion/kineticSweetBusiness200.html', // Auto - Exotic
+//             'infusion/kineticOriginStory306Dupe.html',   // Auto - Legendary
+//             'infusion/kineticOriginStory306Dupe.html',   // Auto - Legendary
+//           );
+//         });
+//         it('should get a fodder indicator', function() {
+//           fateBus.publish(brunchModule, 'fate.infusionCalculated');
+//           expect($('[data-fate-weapon-name="Origin Story"] .fate-fodder.fate-middling.fate-glyph.fglyph-up')).toBeVisible();
+//         });
+//       });
+//
+//       describe('when i am higher light, not junk and not a dupe', function() {
+//         beforeEach(function() {
+//           loadFixtures(
+//             'infusion/energyPerseverance300.html', // Auto - Legendary
+//             'infusion/kineticOriginStory306.html', // Auto - Legendary
+//           );
+//         });
+//         it('should not get a fodder icon because i am the last of my kind', function() {
+//           fateBus.publish(brunchModule, 'fate.infusionCalculated');
+//           expect($('[data-fate-weapon-name="Origin Story"] .fate-fodder.fate-middling.fate-glyph.fglyph-up')).not.toBeVisible();
+//         });
+//       });
+//
+//       describe('mouse interaction', function() {
+//
+//         beforeEach(function() {
+//           loadFixtures(
+//             'infusion/kineticSurosThrowback200.html', // Auto - Uncommon
+//             'infusion/kineticSweetBusiness200.html',  // Auto - Exotic
+//             'infusion/energyPerseverance300.html',    // Auto - Legendary
+//             'infusion/energySolemnHymn300.html',      // Auto - Junk
+//             'infusion/kineticCuboidARu303.html',      // Auto - Rare
+//             'infusion/kineticOriginStory306.html',    // Auto - Legendary
+//           );
+//         });
+//
+//         it('should show other items of the same type with lower light', function() {
+//           fateBus.publish(brunchModule, 'fate.infusionCalculated');
+//
+//           fateBus.publish(brunchModule, 'fate.test.mouseenter.fodder', '[data-fate-weapon-name="Origin Story"] .fate-fodder');
+//
+//           expect($('[data-fate-weapon-name="Suros Throwback"]')).toHaveClass('fate-search-hidden');
+//           expect($('[data-fate-weapon-name="Sweet Business"]')).not.toHaveClass('fate-search-hidden');
+//           expect($('[data-fate-weapon-name="Perseverance"]')).not.toHaveClass('fate-search-hidden');
+//           expect($('[data-fate-weapon-name="Solemn Hymn"]')).toHaveClass('fate-search-hidden');
+//           expect($('[data-fate-weapon-name="Cuboid ARu"]')).toHaveClass('fate-search-hidden');
+//           expect($('[data-fate-weapon-name="Origin Story"]')).not.toHaveClass('fate-search-hidden');
+//
+//           fateBus.publish(brunchModule, 'fate.test.mouseleave.fodder', '[data-fate-weapon-name="Origin Story"] .fate-fodder');
+//
+//           expect($('[data-fate-weapon-name]')).not.toHaveClass('fate-search-hidden');
+//         });
+//
+//       });
+//
+//     });
+//
+//   });
+//
+// });
