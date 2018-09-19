ac431a9568710a09fe28bd2f7e9c1d17511436e8
Robert Slifka <robert.slifka@gmail.com>
2018-09-19 09:07:43 -0700

    Fix Issue #82 - Remove subclass icons

--- app/beautification.js
+++ app/beautification.js
@@ -1,3 +1,9 @@
+const $ = require('jquery');
+
 fateBus.subscribe(module, 'fate.init', function() {
   GM_addStyle(GM_getResourceText('fateOfAllFoolsCSS'));
+});
+
+fateBus.subscribe(module, 'fate.refresh', function() {
+  $('.item-img.diamond').parents('.store-row').attr('style', 'display:none');
 });
--- spec/javascripts/beautification.spec.js
+++ spec/javascripts/beautification.spec.js
@@ -3,9 +3,11 @@ describe('beautification.js', function() {
   const fateBus = require('fateBus.js');
   const brunchModule = {id:'test'+this.result.description};
 
-  it('should apply our custom CSS', function() {
+  beforeEach(function() {
     fateBus.registerModule(brunchModule);
+  });
 
+  it('should apply our custom CSS', function() {
     spyOn(window, 'GM_getResourceText').and.returnValue('TEST_CUSTOM_CSS');
     spyOn(window, 'GM_addStyle');
 
@@ -13,6 +15,14 @@ describe('beautification.js', function() {
 
     expect(window.GM_getResourceText).toHaveBeenCalledWith('fateOfAllFoolsCSS');
     expect(window.GM_addStyle).toHaveBeenCalledWith('TEST_CUSTOM_CSS');
+  });
+
+  it('should remove the subclass icons', function() {
+    loadFixtures(
+      'inventoryContentReact.html',
+    );
+    fateBus.publish(brunchModule, 'fate.refresh');
+    expect($('.item-img.diamond').parents('.store-row')).toBeHidden();
   });
 
 });
