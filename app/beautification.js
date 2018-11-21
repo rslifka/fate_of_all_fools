bbef99325affa6ce7c06ced28b6643a823889573
Robert Slifka <robert.slifka@gmail.com>
2018-11-21 10:00:41 -0800

    Remove the weapons title bar

--- app/beautification.js
+++ app/beautification.js
@@ -4,6 +4,12 @@ fateBus.subscribe(module, 'fate.init', function() {
   GM_addStyle(GM_getResourceText('fateOfAllFoolsCSS'));
 });
 
+// Remove the subclass icons
 fateBus.subscribe(module, 'fate.refresh', function() {
   $('[title~="Subclass"]').parents('.store-row').siblings('.title:contains(Weapons)').siblings('.store-row:eq(0)').attr('style', 'display:none');
+});
+
+// Remove the "Weapons" title bar
+fateBus.subscribe(module, 'fate.refresh', function() {
+  $('[title~="Subclass"]').parents('.store-row').siblings('.title:contains(Weapons)').attr('style', 'display:none');
 });
