66bd2e618c6d441269be996a493c8887d858371f
Robert Slifka <robert.slifka@gmail.com>
2018-12-29 14:42:29 -0800

    Suppress warning message from unqualified file include

--- karma.conf.js
+++ karma.conf.js
@@ -9,6 +9,12 @@ module.exports = function(config) {
       'spec/helpers/!(tampermonkeyStubs).js',
       {pattern: 'spec/javascripts/fixtures/**/*.html', included: false},
       'spec/javascripts/*.spec.js'
-    ]
+    ],
+    /*
+      Suppress "29 12 2018 11:12:44.491:WARN [web-server]: 404: /static/vault-background-a7a8af.png"
+    */
+    proxies: {
+      '/static/vault-background-a7a8af.png': '/app/assets/images/vault-background.png'
+    }
   })
 };
