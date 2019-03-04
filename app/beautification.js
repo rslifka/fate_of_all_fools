a6971d097a2362f091a598a4b0443f3f76ddf443
Robert Slifka <robert.slifka@gmail.com>
2019-03-03 16:23:29 -0800

    Remove the weapons section title and the subclass icons

--- app/beautification.js
+++ app/beautification.js
@@ -6,10 +6,11 @@ fateBus.subscribe(module, 'fate.init', function() {
 
 // Remove the subclass icons
 fateBus.subscribe(module, 'fate.refresh', function() {
-  $('[title~="Subclass"]').parents('.store-row').siblings('.title:contains(Weapons)').siblings('.store-row:eq(0)').attr('style', 'display:none');
+  // AFAIK this is a serial number that shouldn't change between versions...?
+  $('.bucket-3284755031').parents('.store-row').attr('style', 'display:none');
 });
 
 // Remove the "Weapons" title bar
 fateBus.subscribe(module, 'fate.refresh', function() {
-  $('[title~="Subclass"]').parents('.store-row').siblings('.title:contains(Weapons)').attr('style', 'display:none');
+  $('.inventory-title:contains("Weapons")').attr('style', 'display:none');
 });
