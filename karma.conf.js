4c524db0a2efc37ee2a9760e15c134da30fd494c
Robert Slifka <robert.slifka@gmail.com>
2019-06-24 06:25:07 -0700

    Update to more recent karma-jasmine adapter

--- karma.conf.js
+++ karma.conf.js
@@ -1,7 +1,8 @@
 module.exports = function(config) {
   config.set({
     basePath: '.',
-    frameworks: ['jquery-3.2.1', 'jasmine-jquery', 'jasmine'],
+    frameworks: ['jasmine-jquery'],
+    plugins: ['@metahub/karma-jasmine-jquery', 'karma-*'],
     browsers: ['ChromeHeadless'],
     files: [
       'spec/helpers/tampermonkeyStubs.js',
@@ -12,9 +13,11 @@ module.exports = function(config) {
     ],
     /*
       Suppress "29 12 2018 11:12:44.491:WARN [web-server]: 404: /static/vault-background-a7a8af.png"
+      Suppress "24 06 2019 06:06:49.978:WARN [web-server]: 404: /static/legendaryShards-ccf1b4.png"
     */
     proxies: {
-      '/static/vault-background-a7a8af.png': '/app/assets/images/vault-background.png'
+      '/static/vault-background-a7a8af.png': '/app/assets/images/vault-background.png',
+      '/static/vault-background-ccf1b4.png': '/app/assets/images/legendaryShards.png'
     }
   })
 };
