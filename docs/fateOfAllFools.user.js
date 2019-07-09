472b419e20fdbab624a0e82592719074e676fcd4
Travis CI <travis@travis-ci.org>
2019-07-09 12:28:42 +0000

    [skip ci] Publishing build 2644

--- docs/fateOfAllFools.user.js
+++ docs/fateOfAllFools.user.js
@@ -19,5 +19,5 @@
 // @run-at      document-start
 // @supportURL  https://github.com/rslifka/fate_of_all_fools/issues
 // @updateURL   https://openuserjs.org/meta/rslifka/FateOfAllFools_-_DIM_Customization.meta.js
-// @version     2643
+// @version     2644
 // ==/UserScript==
