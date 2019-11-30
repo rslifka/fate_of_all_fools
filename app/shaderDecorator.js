b9a1be403d190199cf23e39b9e3dbf5a8123d4e5
Robert Slifka <robert.slifka@gmail.com>
2019-11-30 12:06:07 -0800

    Fix shader quantity indication

--- app/shaderDecorator.js
+++ app/shaderDecorator.js
@@ -44,11 +44,21 @@ function storeComments() {
   unnecessary contrast when looking at blocks of shaders.
 */
 function oneIsTheLoneliestNumber() {
+  const STAT_CONTAINER_CLASS = '_3TPBp';
+  const STAT_PLACEHOLDER_CLASS = 'data-fate-shader-placeholder'
   $('[data-fate-shader-name]').each(function(index,element) {
-    if ($(this).has('._3TPBp').length > 0) {
-      return;
+    switch($(this).children('.' + STAT_CONTAINER_CLASS).length) {
+      case 0:
+        $(this).append($('<div class="'+STAT_CONTAINER_CLASS+' '+STAT_PLACEHOLDER_CLASS+'"><div class="AtD93"><span>1</span></div></div>'));
+        break;
+      case 2:
+        // If we acquire more shaders than one, DIM will a second container (as
+        // it should!) so lets remove ours.
+        $('.' + STAT_PLACEHOLDER_CLASS).remove();
+        break;
+      default:
+        return;
     }
-    $(this).append($('<div class="_3TPBp"><div class="AtD93"><span>1</span></div></div>'));
   });
 }
 
