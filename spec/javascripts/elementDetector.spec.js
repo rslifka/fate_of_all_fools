a21b4b5fc535d5a7c7a44f68fd48624578bdbc76
Robert Slifka <robert.slifka@gmail.com>
2021-03-07 16:53:53 -0800

    Update Arc image

--- spec/javascripts/elementDetector.spec.js
+++ spec/javascripts/elementDetector.spec.js
@@ -4,7 +4,7 @@ describe('elementDetector.js', function() {
   const brunchModule = {id:'test'+this.result.description};
   const elementDetector = require('elementDetector.js');
 
-  const ARC_DAMAGE_ICON = 'https://www.bungie.net/common/destiny2_content/icons/DestinyDamageTypeDefinition_9fbcfcef99f4e8a40d8762ccb556fcd4.png';
+  const ARC_DAMAGE_ICON = 'https://www.bungie.net/common/destiny2_content/icons/DestinyDamageTypeDefinition_092d066688b879c807c3b460afdd61e6.png';
   const SOLAR_DAMAGE_ICON = 'https://www.bungie.net/common/destiny2_content/icons/DestinyDamageTypeDefinition_2a1773e10968f2d088b97c22b22bba9e.png';
   const STASIS_DAMAGE_ICON = 'https://www.bungie.net/common/destiny2_content/icons/DestinyDamageTypeDefinition_530c4c3e7981dc2aefd24fd3293482bf.png';
   const VOID_DAMAGE_ICON = 'https://www.bungie.net/common/destiny2_content/icons/DestinyDamageTypeDefinition_ceb2f6197dccf3958bb31cc783eb97a0.png';
