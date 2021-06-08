fe1a255958e107e8b773653c0eae8458c9c9fe1d
Travis CI <travis@travis-ci.org>
2021-06-08 12:40:23 +0000

    [skip ci] Publishing build 2772

--- docs/fateOfAllFools.user.js
+++ docs/fateOfAllFools.user.js
@@ -20,5 +20,5 @@
 // @run-at      document-start
 // @supportURL  https://github.com/rslifka/fate_of_all_fools/issues
 // @updateURL   https://openuserjs.org/meta/rslifka/FateOfAllFools_-_DIM_Customization.meta.js
-// @version     2770
+// @version     2772
 // ==/UserScript==
