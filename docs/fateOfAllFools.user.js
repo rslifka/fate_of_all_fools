66eba9c2b3a4c05c41ae2c4808ae55ceed918c9c
Circle CI <circleci@circleci.com>
2022-06-16 21:44:16 +0000

    [skip ci] Publishing build 27

--- docs/fateOfAllFools.user.js
+++ docs/fateOfAllFools.user.js
@@ -1,4 +1,5 @@
 // ==UserScript==
+// @name        FateOfAllFools - DIM Customization
 // @author      rslifka
 // @connect     docs.google.com
 // @connect     googleusercontent.com
@@ -14,11 +15,10 @@
 // @homepageURL https://github.com/rslifka/fate_of_all_fools
 // @license     MIT; https://raw.githubusercontent.com/rslifka/fate_of_all_fools/master/LICENSE.txt
 // @match       https://*.destinyitemmanager.com/*
-// @name        FateOfAllFools - DIM Customization
 // @require     https://rslifka.github.io/fate_of_all_fools/fateOfAllFools.js
 // @resource    fateOfAllFoolsCSS https://rslifka.github.io/fate_of_all_fools/fateOfAllFools.css
 // @run-at      document-start
 // @supportURL  https://github.com/rslifka/fate_of_all_fools/issues
 // @updateURL   https://openuserjs.org/meta/rslifka/FateOfAllFools_-_DIM_Customization.meta.js
-// @version     253000
+// @version     273000
 // ==/UserScript==
