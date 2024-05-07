e61b53f933b0678ffc91058b7adcc2a97d178f49
Rob S <rslifka@users.noreply.github.com>
2024-05-06 17:31:47 -0700

    Fix compilation

--- spec/javascripts/infusionIndicator.spec.js
+++ spec/javascripts/infusionIndicator.spec.js
@@ -1,7 +1,7 @@
 xdescribe('infusionIndicator.js', function() {
 
   const fateBus = require('fateBus.js');
-  const brunchModule = {id:'test'+this.result.description};
+  const brunchModule = {id:'test'+this.description};
 
   beforeEach(function() {
     fateBus.registerModule(brunchModule);
