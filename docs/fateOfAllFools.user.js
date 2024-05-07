4c7dbb7d4856292e94bcf412cc1ec7fb9613c107
Circle CI <circleci@circleci.com>
2024-05-07 01:57:12 +0000

    [skip ci] Publishing build 49

--- docs/fateOfAllFools.user.js
+++ docs/fateOfAllFools.user.js
@@ -20,5 +20,5 @@
 // @run-at      document-start
 // @supportURL  https://github.com/rslifka/fate_of_all_fools/issues
 // @updateURL   https://openuserjs.org/meta/rslifka/FateOfAllFools_-_DIM_Customization.meta.js
-// @version     473000
+// @version     493000
 // ==/UserScript==
