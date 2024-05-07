e4517341645b0cc2971dbf71ba2e347b3c4f7a2b
Circle CI <circleci@circleci.com>
2024-05-07 01:53:14 +0000

    [skip ci] Publishing build 47

--- docs/fateOfAllFools.user.js
+++ docs/fateOfAllFools.user.js
@@ -4,7 +4,7 @@
 // @connect     docs.google.com
 // @connect     googleusercontent.com
 // @connect     rslifka.github.io
-// @copyright   2023, Robert Slifka (https://github.com/rslifka/fate_of_all_fools)
+// @copyright   2024, Robert Slifka (https://github.com/rslifka/fate_of_all_fools)
 // @description Enhancements to the Destiny Item Manager
 // @grant       GM_addStyle
 // @grant       GM_getResourceText
@@ -14,11 +14,11 @@
 // @grant       GM_xmlhttpRequest
 // @homepageURL https://github.com/rslifka/fate_of_all_fools
 // @license     MIT; https://raw.githubusercontent.com/rslifka/fate_of_all_fools/master/LICENSE.txt
-// @match       https://*.destinyitemmanager.com/*
+// @match       https://app.destinyitemmanager.com/*
 // @require     https://rslifka.github.io/fate_of_all_fools/fateOfAllFools.js
 // @resource    fateOfAllFoolsCSS https://rslifka.github.io/fate_of_all_fools/fateOfAllFools.css
 // @run-at      document-start
 // @supportURL  https://github.com/rslifka/fate_of_all_fools/issues
 // @updateURL   https://openuserjs.org/meta/rslifka/FateOfAllFools_-_DIM_Customization.meta.js
-// @version     453000
+// @version     473000
 // ==/UserScript==
